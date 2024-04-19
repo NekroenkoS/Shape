383a037198bbf8aebc0899297cf8f8081add6242
Sergey Nekroenko <sergeyn213@gmail.com>
2024-04-19 21:29:08 +0300

    Initial commit

---
+++ src/example/Rectangle.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Rectangle extends Shape{
+
+    double length;
+    double width;
+
+    public Rectangle (double length, double width){
+
+        this.length = length;
+        this.width=width;
+    }
+
+    @Override
+    public double getArea(){
+
+return length*width;
+    }
+}
---
+++ src/example/Shape.java
@@ -0,0 +1,8 @@
+package example;
+
+public class Shape {
+
+public double getArea(){
+    return 0.0;
+}
+}
